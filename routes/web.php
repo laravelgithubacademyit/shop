aa4be69443f51b15a9dc0f3e359a96aa286bb5e9
laravelgithubacademyit <laravelgithubacademyit@gmail.com>
2021-01-18 21:56:32 +0330

    web templates added to the project.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
+});
+
+
+Route::get('/adminpanel', function() {
+    return view('admin.home');
 });
